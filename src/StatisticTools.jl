4cd3b4a83b70b79d77f6f9e6adf7e8a32b4a68f3
Johannes Helmes <helmes@thp.uni-koeln.de>
2016-03-30 21:36:51 +0200

    first implementation of weightedmean

--- src/StatisticTools.jl
+++ src/StatisticTools.jl
@@ -1,5 +1,19 @@
 module StatisticTools
 
-# package code goes here
+export weightedmean
+
+"""
+weightedmean(values::Array{Float64,1}, errors::Array{Float64,1})
+
+computes the weighted mean of values, using as weights w = 1/ sigma^2
+"""
+function weightedmean(x::Array{Float64,1}, errors::Array{Float64,1}) 
+    @assert length(x) == length(errors)
+    weights = 1./ (errors.^2)
+    sigma = sqrt( 1./ sum( weights ) )
+    mean = sigma^2 * sum(x .* weights)
+
+    mean, sigma
+end
 
 end # module
